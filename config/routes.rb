c218b77c571c0615eca46297eec56352713b2f78
justingarcia <justgarcia5@gmail.com>
2020-06-01 16:33:16 -0700

    fixed lost work from merge issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { 
+  devise_for :users, :controllers => {
     omniauth_callbacks: "users/omniauth_callbacks",
     sessions: "users/sessions",
     registrations: "users/registrations"
   }
   resources :products
+  resources :conversations do
+    resources :messages
+  end
   get '/auth/facebook/callback' => 'sessions#create'
   get '/my-products', to: 'pages#my-products'
   get '/*path', to: 'pages#index', constraints: ->(request){ request.format.html? }
