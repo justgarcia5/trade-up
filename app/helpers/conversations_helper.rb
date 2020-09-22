08f1afcd1eda1f16fe9e716ac58a7d47162a774f
justingarcia <justgarcia5@gmail.com>
2020-09-22 11:57:44 -0700

    showing last message in conversations list

---
+++ app/helpers/conversations_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module ConversationsHelper
+
+  def conversation_last_message(id)
+    messages = Message.where(conversation_id: id)
+    messages.last.body
+  end
+
+end
--- app/javascript/components/NavBar.js
+++ app/javascript/components/NavBar.js
@@ -6,13 +6,18 @@ const NavBar = props => {
   const [displayAlertClass, setDisplayAlertClass] = useState('')
 
   useEffect(() => {
-    fetch(`/notifications.json`)
-      .then(response => response.json())
-      .then(notifications => {
-        setNotifications(notifications);
-        let alertClass = notifications.length > 0 ? "message_alert" : " "
-        setDisplayAlertClass(alertClass);
-      });
+    async function fetchData() {
+      const res = await fetch(`/notifications.json`);
+        res
+          .json()
+          .then(notifications => {
+            setNotifications(notifications);
+            let alertClass = notifications.length > 0 ? "message_alert" : " "
+            setDisplayAlertClass(alertClass);
+          });
+    }
+
+    fetchData();
   }, []);
 
   const handleClick = () => {
@@ -23,8 +28,8 @@ const NavBar = props => {
       setNotifications(notifications)
     })
   }
-  const notificationDisplay = notifications.length > 0 ? notifications.length : " ";
 
+  const notificationDisplay = notifications.length > 0 ? notifications.length : " ";
   return(
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -32,11 +37,6 @@ const NavBar = props => {
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-
-        {/* <form className="form-inline m-auto ">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form> */}
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
